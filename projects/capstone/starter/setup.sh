1ed52d023ddbf3ed321e8f84e6bf9e5ba0c61dbc
showall <gerardsho@gmail.com>
2022-07-13 02:34:03 +0800

    launched to github

--- projects/capstone/starter/setup.sh
+++ projects/capstone/starter/setup.sh
@@ -1,1 +1,3 @@
-
+export DATABASE_URL="postgres://noszvnjwsurtga:27fec1fbb91fe13739e6ba56a7399caef181e23f237434a0e8916cc878883915@ec2-3-223-169-166.compute-1.amazonaws.com:5432/d3k0ua61ao28j5"
+export EXCITED="true"
+echo "setup.sh script executed successfully!"
